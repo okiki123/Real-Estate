bb5eb3cdf4f6743f941a8cd9d9b8f95fac22cf50
okiki123 <okikimalomo17@gmail.com>
2022-01-07 21:22:14 +0100

    Updated Commit

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import Image from 'next/Image';
+import Image from 'next/image';
 import { Box, Flex, Text, Avatar } from '@chakra-ui/react';
 import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,6 @@ const Banner = ({purpose, title1, title2, desc1, desc2, buttonText, linkName, im
 export default function Home({ propertiesForSale, propertiesForRent }) {
   return (
     <Box>
-      <h1>Hello World</h1>
       <Banner 
       purpose="RENT A HOME" 
       title1="Rental Homes for"
@@ -44,7 +43,9 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
       linkName="/search?purpose=for-sale"
       imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008" 
       />
+      <Flex flexWrap="wrap">
       {propertiesForSale.map((property) => <Property property={property} key={property.id}/>)}
+      </Flex>
     </Box>
   )
 }
